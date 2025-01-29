234a0f57181c3e14330bd9680a5afcdc54c6dbe1
Keshava Nandu Gubba <nandu.gubba@gmail.com>
2025-01-29 21:20:53 +0530

    Task 3: Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -1,6 +1,7 @@
 describe('When: I use the reading list feature', () => {
   beforeEach(() => {
     cy.startAt('/');
+    readingItemsCount = cy.$$('[data-testing="reading-list-item"]').length;
   });
 
   it('Then: I should see my reading list', () => {
@@ -11,4 +12,40 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should add the book to Reading list and undo it', () => {
+    cy.get('input[type="search"]').type('python');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="add-item"]:enabled').first().click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
+
+    cy.get('.mat-simple-snackbar-action button').click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount);
+  });
+
+  it('Then: I should remove item from Reading list and undo it', () => {
+    cy.get('input[type="search"]').type('python');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="add-item"]:enabled').first().click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
+
+    cy.get('[data-testing="toggle-reading-list"]').click();
+
+    cy.get('[data-testing="remove-item"]:enabled').first().click();
+
+    cy.get('[data-testing="reading-list-close"]').click();
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount);
+
+    cy.get('.mat-simple-snackbar-action button').click({force: true});
+
+    cy.get('[data-testing="reading-list-item"]').should('have.length', readingItemsCount + 1);
+  })
 });
